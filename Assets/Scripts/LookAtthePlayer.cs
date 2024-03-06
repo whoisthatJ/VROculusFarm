b326806a47ec5357929de6f4c8260eb9e18e5580
whoisthatJ <Marat.J.Urmanov@gmail.com>
2024-03-06 13:51:18 +0600

    Fixed characters look at player

--- Assets/Scripts/LookAtthePlayer.cs
+++ Assets/Scripts/LookAtthePlayer.cs
@@ -8,7 +8,7 @@ public class LookAtthePlayer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player");
+        player = GameObject.FindGameObjectWithTag("MainCamera");
     }
 
     // Update is called once per frame
