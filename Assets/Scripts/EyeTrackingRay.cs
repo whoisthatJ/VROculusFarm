97e68b816e2be28ae2678948518e86e8a75547b6
arrietta <39980503+arrietta@users.noreply.github.com>
2025-02-09 17:26:30 +0500

    [FIX] eye contact fix

--- Assets/Scripts/EyeTrackingRay.cs
+++ Assets/Scripts/EyeTrackingRay.cs
@@ -10,6 +10,7 @@ public class EyeTrackingRay : MonoBehaviour
     private float rayDistance = 5f;
     private LayerMask layerToInclude;
     private LineRenderer lineRenderer;
+    [SerializeField] Transform base_point;
     [SerializeField]
     private Color rayColorDefault = Color.yellow;
     [SerializeField]
@@ -28,14 +29,15 @@ public class EyeTrackingRay : MonoBehaviour
         lineRenderer.endWidth = rayWidth;
         lineRenderer.startColor = rayColorDefault;
         lineRenderer.endColor = rayColorDefault;
-        lineRenderer.SetPosition(0, transform.position);
-        lineRenderer.SetPosition(1, transform.position+Vector3.forward * rayDistance);
-
+        lineRenderer.SetPosition(0, new Vector3(transform.position.x,0,transform.position.z));
+        lineRenderer.SetPosition(1, new Vector3(transform.position.x,transform.localPosition.y,transform.position.z + rayDistance));
+        /*        lineRenderer.SetPosition(1, transform.position+Vector3.forward * rayDistance);*/
     }
 
     void FixedUpdate()
     {
         RaycastHit hit;
+        transform.position = base_point.transform.position;
         Vector3 raycastDirection = transform.TransformDirection(Vector3.forward) * rayDistance;
         if (Physics.Raycast(transform.position, raycastDirection, out hit)){//, Mathf.Infinity, layerToInclude)) {
             lineRenderer.startColor = rayColorHover;
